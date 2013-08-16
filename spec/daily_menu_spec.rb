42c81bd8ccc8e032212522f761640af493407675
KARASZI István <github@spam.raszi.hu>
2013-08-16 17:37:52 +0200

    tests for .menus_for

--- spec/daily_menu_spec.rb
+++ spec/daily_menu_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe DailyMenu do
+  let(:location) { 'City/Area' }
+
   describe 'ROOT' do
     it 'should return the root path of the gem' do
       expected_path = File.expand_path('..', File.dirname(__FILE__))
@@ -20,14 +22,14 @@ describe DailyMenu do
     end
 
     it 'should raise an error when the config file not found or not readable' do
-      expect { described_class.restaurants_for('Foo/Bar') }.to raise_error
+      expect { described_class.restaurants_for(location) }.to raise_error
     end
 
     it 'should read the configuration file' do
       described_class.stub(:file_accessible?) { true }
       YAML.stub(:load_file) { [] }
 
-      described_class.restaurants_for('City/Area')
+      described_class.restaurants_for(location)
 
       expect(YAML).to have_received(:load_file).with(%r(City/Area\.yml$))
     end
@@ -35,10 +37,31 @@ describe DailyMenu do
     it 'should return with Restaruants' do
       described_class.stub(:config_for) { [{ name: 'Test', scraper: { class: 'Facebook', params: '1', }, filter: { class: 'Hungarian' } }] }
 
-      restaurants = described_class.restaurants_for('City/Area')
+      restaurants = described_class.restaurants_for(location)
 
       expect(restaurants).to have(1).restaurant
       expect(restaurants[0]).to be_a(DailyMenu::Restaurant)
+    end
+  end
+
+  describe '.menus_for' do
+    it 'should get the restaurants' do
+      described_class.stub(:restaurants_for) { [] }
+
+      described_class.menus_for(location)
+
+      expect(described_class).to have_received(:restaurants_for).with(location)
+    end
+
+    it 'should return with correct items' do
+      menu = double('Menu')
+      restaurant = double('Restaurant', menu: menu)
+      described_class.stub(:restaurants_for) { [restaurant] }
+
+      menus = described_class.menus_for(location)
+
+      expect(menus).to have(1).menus
+      expect(menus).to include([restaurant, menu])
     end
   end
 

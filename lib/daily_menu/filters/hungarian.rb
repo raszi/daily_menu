2f3f3ba9778fef11da3e29bbade7f634ffb5ed32
KARASZI István <github@spam.raszi.hu>
2013-10-10 23:08:18 +0200

    catch prices as well

--- lib/daily_menu/filters/hungarian.rb
+++ lib/daily_menu/filters/hungarian.rb
@@ -4,7 +4,7 @@ module DailyMenu::Filters
     def_delegator :@filter, :matches?
 
     def initialize
-      @filter = DailyMenu::Filters::Regexp.new(/\b(?:menü|ebéd|leves)/i)
+      @filter = DailyMenu::Filters::Regexp.new(/\b(?:menü|ebéd|leves|\d+.-)/i)
     end
   end
 end
